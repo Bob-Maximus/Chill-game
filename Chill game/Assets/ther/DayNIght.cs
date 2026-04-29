afe5213bd9e41385d04ae030ef920f6f0c40c827
Bob-Maximus <1elkabbanyou@hdsb.ca>
2026-04-28 22:28:22 -0400

    added day night cycle

--- Chill game/Assets/ther/DayNIght.cs
+++ Chill game/Assets/ther/DayNIght.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class DayNIght : MonoBehaviour
 {
-    public float timeOfDay;
-    public Material sky;
+    public float time;
+    private Material sky;
+    public Transform sun;
+    public float daySpeed;
 
+    public float timeOfDay;
+
+    public void Awake()
+    {
+        sky = RenderSettings.skybox;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        timeOfDay+=Time.deltaTime;
-        float weather = timeOfDay;
-        sky.SetFloat("Atmosphere Thickness", weather);
+        timeOfDay+=Time.deltaTime*daySpeed;
+        //timeOfDay = time;
+
+        if (timeOfDay > 24)
+        {
+            timeOfDay=0;
+        }
+
+        //sun.transform.rotation = Quaternion.Euler(timeOfDay*15, sun.transform.rotation.eulerAngles.y, sun.transform.rotation.eulerAngles.z);
+        sun.transform.Rotate(Time.deltaTime*daySpeed*15, 0, 0);
+        //sky.SetFloat("_AtmosphereThickness", timeOfDay);
     }
 }
